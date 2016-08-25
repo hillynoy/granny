c170054be8f71013ce45f4a1d8da8e57c000a2b5
hilly noy <hilly88@gmail.com>
2016-08-25 11:22:52 +0300

    Initial commit

---
+++ content.js
@@ -0,0 +1,36 @@
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].appendChild("" +
+      "");
+}
+
+
+
+
+//left column
+var element = document.querySelector('#leftCol');
+element.parentElement.removeChild(element);
+
+//right colomn
+var element = document.querySelector('#rightCol');
+element.parentElement.removeChild(element);
+
+var element = document.querySelector('._4-u2.mbm._5v6e._4-u8');
+element.parentElement.removeChild(element);
+
+var element = document.querySelector('.home_right_column');
+element.parentElement.removeChild(element);
+
+
+//ad menu
+var element = document.querySelector('.clearfix._3tx');
+element.parentElement.removeChild(element);
+
+var element = document.querySelector('#u_0_1h');
+element.parentElement.removeChild(element);
+
+
+//middle feed
+var element = document.querySelector('.clearfix._3tx');
+
+_1dwg._1w_m
